b5414e2f706562db9f7014a2b71d48d0182f1d3f
Indra Wahyu Tirtayasa <indrawahyu856@gmail.com>
2026-09-11 15:18:04 +0700

    Door Implementation

---
+++ Assets/Game/Scripts/Interactable/Door/Door.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Door : MonoBehaviour, IInteractable
+{
+    [SerializeField] private string _name;
+    [SerializeField] protected Transform _doorTransform;
+    [SerializeField] protected float _duration = 1f;
+    [SerializeField] protected bool _islocked;
+    [SerializeField] protected string _keyID;
+    protected bool _isAnimating;
+    protected bool _isOpen;
+    protected Coroutine _animatingDoorCoroutine;
+    public bool IsAnimating => _isAnimating;
+    public string Name => _name;
+    public UnityEvent OnDoorOpen;
+    public UnityEvent OnDoorClose;
+    [ContextMenu("Interact Door")]
+    public void Interact()
+    {
+        if (_isOpen == true)
+        {
+            Close();
+        }
+        else
+        {
+            Open();
+        }
+    }
+    public virtual void Open()
+    {
+        _isOpen = true;
+        OnDoorOpen?.Invoke();
+    }
+    public virtual void Close()
+    {
+        _isOpen = false;
+        OnDoorClose?.Invoke();
+    }
+}
