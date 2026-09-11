958b0233dfc811716b67c93f430a8ba0694dc0ba
Indra Wahyu Tirtayasa <indrawahyu856@gmail.com>
2026-09-11 10:40:34 +0700

    Interface Interactable & Pickable

---
+++ Assets/Game/Scripts/Interactable/Door.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+public class Door : MonoBehaviour, IInteractable
+{
+    [SerializeField] private string _name;
+    public string Name => _name;
+    public void Interact()
+    {
+
+    }
+}
